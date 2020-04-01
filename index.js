2d09b1733431231d8f658324062f7ba63de1bba2
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-04-01 04:06:20 -0400

    sets an array of song objects

---
+++ index.js
@@ -0,0 +1,37 @@
+let songs = [
+	{
+        artistName: "Queen",
+	    songName: "Bohemian Rhapsody - Sample",
+        url: "https://upload.wikimedia.org/wikipedia/en/f/fb/QueenBohemianRhapsody_Opera.ogg"
+    },
+	{
+        artistName: "Queen",
+	    songName: 'We Will Rock You - Sample',
+        url: "https://upload.wikimedia.org/wikipedia/en/b/bc/Wewillrockyou.ogg"
+    },
+	{
+        artistName: "Queen",
+	    songName: "Don't Stop Me Know - Sample",
+        url: "https://upload.wikimedia.org/wikipedia/en/d/db/Queen_-_Don%27t_Stop_Me_Now.ogg"
+    },
+	{
+        artistName: "Queen",
+	    songName: "I Want To Break Free - Sample",
+	    url: "https://upload.wikimedia.org/wikipedia/en/7/75/Queen_I_want_to_break_free.ogg"},
+	{
+        artistName: "Queen",
+	    songName: "Another One Bites The Dust - Sample",
+        url: "https://upload.wikimedia.org/wikipedia/en/3/39/Another-one-bites-the-dust--forward.ogg"
+    },
+	{
+        artistName: "Queen",
+	    songName: "We Are The Champions - Sample",
+        url: "https://upload.wikimedia.org/wikipedia/en/1/1c/Wearethechampions.ogg"
+    },
+	{
+        artistName: "Queen",
+	    songName: "Crazy Little Thing Called Love - Sample",
+        url: "https://upload.wikimedia.org/wikipedia/en/0/06/Queen_-_Crazy_Little_Thing_Called_Love.ogg"
+    }
+]
+
